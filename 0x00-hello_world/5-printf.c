46476a4c105abc9ed65252219368cf1c9faeedd3
alexkariuki2000 <alexkamunge@gmail.com>
2022-06-02 15:47:10 -0700

    cproject

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
+
 /**
+ * main - Prints out a string to stdout.
  *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *
- *    */
+ * Return: Always 0.
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
+
 	return (0);
 }
