60d7fd8200efa8d123cb24f0ee4677b132e36706
alexkariuki2000 <alexkamunge@gmail.com>
2022-06-02 15:25:41 -0700

    cproject

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- *
- *  * Description: main - Prints out a string to stdout.
- *
- *   * Return: 0 if success.
- *    */
+* main - Prints "Programming is like building a multilingual
+*		 puzzle, followed by a new line."
+*
+* Return: Always 0.
+* */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
