e3b698c0e8cd422bba95d293c1c94cbdf8af5a80
alexkariuki2000 <alexkamunge@gmail.com>
2022-06-03 12:25:52 -0700

    variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -7,7 +7,7 @@
  * if the number is greater than 0: is positive, if the number is 0: is zero
  * if the number is less than 0: is negative, followed by a new line
  *
- * Return: Always 0.
+ * Return: 0
  */
 int main(void)
 {
@@ -29,5 +29,4 @@ int main(void)
 }
 
 	return (0);
-
 }
