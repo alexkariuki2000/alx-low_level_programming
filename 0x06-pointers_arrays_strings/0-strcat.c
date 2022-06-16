d2d2ced4aa52922829032483263535c4c3defb9f
alexkariuki2000 <alexkamunge@gmail.com>
2022-06-16 11:21:44 -0700

    pointers

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,11 +10,10 @@
 char *_strcat(char *dest, char *src)
 {
 	unsigned int i = 0;
-
 	unsigned int j = 0;
 
 	while (*(dest + i) != '\0')
-		i++;
+	i++;
 	while (*(src + j) != '\0')
 {
 	*(dest + i) = *(src + j);	
